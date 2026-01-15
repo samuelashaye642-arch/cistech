d4b8964daa4eb7581d7a6890299fce09dd2b63fe
samuel <samuelashaye642@gmail.com>
2026-01-15 03:31:28 -0700

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,222 @@
+// console.log(1);
+// let a = 5;
+// let year = 2026;
+// console.log(a, year);
+
+// let firstName = "samuel";
+// console.log(firstName);
+
+
+// points = 6;
+// console.log(points);
+
+// day = 7;
+// console.log(day);
+
+// let num1 = 7;
+// let num2 = 8;
+// let sum = num1 + num2;
+// console.log(sum);
+
+// let email = "awet@gmail.com";
+// console.log(email);
+
+// let firstName ="Helen";
+// let lastName = "Munachi";
+// let fullName = firstName  + " " + lastName;
+// console.log(fullName);
+
+// console.log(fullName[2]);
+// console.log(fullName.length);
+// console.log(fullName.toUpperCase());
+
+
+// let age = 18;
+// console.log("my name is Al Arabiy and i am" + age + "years");
+
+
+// let email = "awet@gmail.com";
+// let result = email.lastIndexOf('t');
+// console.log(result);
+
+// let result = email.slice(0,10)
+// console.log(result);
+
+// let result = email.substr(4,7);
+// console.log(result);
+
+// let result = email.replace('m','n');
+// console.log(result);
+
+// let radius = 10;
+// const pi = 3.142;
+// console.log(radius, pi);
+// console.log(10/2);
+
+// let result = pi*radius**2;
+// console.log(result);
+
+
+// let likes = 10;
+// likes*2;
+
+// console.log(likes);
+//  section
+// let x = 10;
+// let y = 5;
+// let sum = x + y;
+// console.log(sum)
+
+// let price = 200;
+// let quantity = 3;
+// let come = price * quantity;
+// console.log(come);
+
+// let a  = 20;
+// let b = 4;
+// console.log(a/b)
+
+//  let num1 = 15;
+//  let num2 = 7;
+//  let money = num1 % num2; 
+//  console.log(money); 
+
+// //  bedmas
+// let f = 10;
+// let v = 5;
+// let g = 2;
+// let get = f + v * 2;
+// console.log(get);
+ 
+// let h = 10;
+// let df = 5;
+// let van = h + df;
+// let fr = 2
+// console.log(van*2)
+
+// let run = 50;
+// let non = 20;
+// let fun = 4;
+// let sol = run - 20 / 4;
+// console.log(sol)
+
+// let ben = 50;
+// let dk = 20;
+// let mom = ben - dk;
+// let we = 4;
+// console.log(mom/4)
+
+// let i = 8;
+// let j = 2;
+// let dd = 5;
+// let gg = 4;
+// let ww = 2;
+// let dad = i + j * dd - gg / ww;
+// console.log(dad);
+
+// let fullName = 'javascript';
+// console.log(fullName.toUpperCase());
+
+// let nn = fullName.substring(4,6);
+// console.log(nn);
+
+// let Name = 'banana';
+// console.log(Name.indexOf("a"));
+// console.log(Name.indexOf('z'));
+
+// let naa = 'computer'
+// console.log(naa.substring(0,4));
+
+// let fub = 'hello world';
+// console.log(fub.toUpperCase());
+// console.log(fub.indexOf('world'));
+
+
+
+// let awet = ['samuel', 'Annabel', 'kaito'];
+// // console.log(awet);
+
+// console.log(awet[1]);
+// awet[1] = 'Helen';
+// let result = awet.join('-');
+// let result = awet.push('david');
+
+
+// console.log(result);
+
+// console.log(true,false);
+
+// let age = 26;
+// console.log(age != 26);
+
+// for(let i = 0; i < 15; i++){
+// console.log(i);
+
+// }
+
+
+
+// let i = 0;
+// while(i < 5){
+// console.log(i);
+// i++;
+// }
+
+
+// let i = 0;
+// do{
+//   console.log(i);
+//   i++;
+// }while(i < 5);
+
+
+// const age = 18;
+//  if (age < 18 ){
+//   console.log("you can vote");
+//  }
+//  else{
+//   console.log("you cant vote");
+//  }
+
+
+// const score = 'D';
+
+// switch(score){
+//   case 'A':
+//     console.log('you got A')
+//     break;
+//   case 'B':
+//     console.log('you got B')
+//     break;
+//   case 'C':
+//     console.log('you got c')
+//     break;
+//   case 'D':
+//     console.log('you got D')
+//     break;
+//   case 'E':
+//     console.log('you gor E')
+//     break;
+//     default:
+//     console.log('no grade');
+
+
+// }
+
+// let nr1 = 10;
+// let nr2 = 3;
+// let result = nr1 % nr2;
+// console.log(result);
+
+// let nr = 2;
+// console.log(nr++);
+// console.log(nr);
+
+
+// let nr1 = 2;
+// console.log(nr1);
+// console.log(++nr1);
+
+
+
+
